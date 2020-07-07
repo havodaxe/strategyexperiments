25aefbd07770c3063c0733612b54cc02ad431c84
havodaxe <havodaxe@gmail.com>
2020-07-07 19:51:30 +0200

    Make function for setup code

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
 import pygame
-pygame.init()
 
-window_size = w_width, w_height = 320, 240
+def main():
+    pygame.init()
+    window_size = w_width, w_height = 320, 240
 
-screen = pygame.display.set_mode(window_size)
+    screen = pygame.display.set_mode(window_size)
 
-while(True):
-    for event in pygame.event.get():
-        if (event.type == pygame.QUIT or
-            event.type == pygame.KEYDOWN and
-            event.key == pygame.K_ESCAPE): exit()
+    while(True):
+        for event in pygame.event.get():
+            if(event.type == pygame.QUIT or
+               event.type == pygame.KEYDOWN and
+               event.key == pygame.K_ESCAPE):
+                exit()
+
+if(__name__ == "__main__"):
+    main()
